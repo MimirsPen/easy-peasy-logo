5dccf854d990e40369607c9acb34fd218fec8dbf
PomPomZak <info@storcklabs.com>
2026-07-13 10:40:56 +0700

    feat: add HelmetProvider for SEO meta tags

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AppStateProvider } from "./state/AppStateProvider";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
-  <AppStateProvider>
-    <App />
-  </AppStateProvider>
+  <HelmetProvider>
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
+  </HelmetProvider>
 );
